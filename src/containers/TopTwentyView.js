12d1fcb92b36ff94bd3dffa704a1a21af070d638
DocLJN <ljnoble@gmail.com>
2018-03-14 22:12:10 +0000

    Top Twenty List inside Top Twenty View displays Hello World

--- src/containers/TopTwentyView.js
+++ src/containers/TopTwentyView.js
@@ -1,11 +1,12 @@
 import React, {Component} from "react";
+import TopTwentyList from "../components/TopTwentyList"
 
 class TopTwentyView extends Component {
 
   render () {
     return (
       <div className="top-twenty-view">
-        Hello World!
+        <TopTwentyList />
       </div>
     );
   }
