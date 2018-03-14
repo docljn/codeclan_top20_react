89ccdd0bb5e56a0584e2ed52ab27dbb53f7c3d54
DocLJN <ljnoble@gmail.com>
2018-03-14 22:44:15 +0000

    Top Twenty Entry displays Hello World when inside Top Twenty List

---
+++ src/components/TopTwentyEntry.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+const TopTwentyEntry = () => {
+  return (
+    "Hello World!"
+  );
+};
+
+export default TopTwentyEntry;
--- src/components/TopTwentyList.js
+++ src/components/TopTwentyList.js
@@ -1,10 +1,11 @@
 import React, {Component} from "react";
+import TopTwentyEntry from "./TopTwentyEntry";
 
 class TopTwentyList extends Component {
   render () {
     return(
       <div>
-        Hello World!
+        <TopTwentyEntry />
       </div>
     );
   }
